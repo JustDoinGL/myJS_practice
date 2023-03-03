9f1ba60f5d27cdc652be82ce7861e18256f8884c
Andrew <andrey.chulkov@list.ru>
2023-03-03 15:36:22 +0300

    First task

---
+++ js/first.js
@@ -0,0 +1,8 @@
+document.onclick = function(event) {
+    if ((event.target.tagName === "IMG") && (event.target.classList.contains('changes-img'))) {
+        event.target.classList.remove('changes-img')
+    }
+    else if (event.target.tagName === "IMG") {
+       event.target.classList.add('changes-img')
+    } 
+}
