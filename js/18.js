f074a392734c2bf4cd3c5400c2ff79dfdff2e853
Andrew <106160775+JustDoinGL@users.noreply.github.com>
2023-03-11 19:19:52 +0300

    Add files via upload

--- js/18.js
+++ js/18.js
@@ -1,16 +1,19 @@
 const words = ['super', 'blin', 'nine', 'flex']
 
-
-
-
-
-
 document.querySelector('#task18__button').onclick = () => {
     let text = document.querySelector('#task18-getText').value
     for (let i = 0; i < words.length; i++) {
         while (text.indexOf(words[i]) != -1) {
-            text = text.replace(words[i], "*")
+            text = text.replace(words[i], star(words[i].length))
         }
     }
     document.querySelector('#task18-text').innerHTML += '<div>' + text + '</div>'
+}
+
+function star(n) {
+    let out = ''
+    for (let i = 0; i < n; i++) {
+        out += '*'
+    }
+    return out
 }
