c1750f95044638157ee8e603b3cf9e78d253ce61
Andrew <106160775+JustDoinGL@users.noreply.github.com>
2023-03-12 13:00:21 +0300

    Add files via upload

--- js/19.js
+++ js/19.js
@@ -1,17 +1,20 @@
 let randomNumber = Math.floor((Math.random() * 10) + 1)
 // console.log(randomNumber)
 
-
-
 document.getElementById('task19__button').onclick = () => {
     let number = Number(document.getElementById('task19__input').value)
-    // console.log(number)
     let out = document.getElementById('task19__out')
-    if (number === randomNumber) {
-        out.innerHTML = 'Вы угадали число!'
-    } else if (number > randomNumber) {
-        out.innerHTML = 'Вы ввели число больше загаданного'
-    } else {
-        out.innerHTML = 'Вы ввели число меньше загаданного'
+    // console.log(number)
+    if ((number > 10) || (number < 0)) {
+        out.innerHTML = 'Вы вели число, которое не соотвествует условию'
+    }
+    else {
+        if (number === randomNumber) {
+            out.innerHTML = 'Вы угадали число!'
+        } else if (number > randomNumber) {
+            out.innerHTML = 'Вы ввели число больше загаданного'
+        } else {
+            out.innerHTML = 'Вы ввели число меньше загаданного'
+        }
     }
 }
