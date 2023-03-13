114644f0ab626cc0824005fae63929f835cd56e0
Andrew <106160775+JustDoinGL@users.noreply.github.com>
2023-03-13 19:57:26 +0300

    Add files via upload

--- js/20.js
+++ js/20.js
@@ -1,16 +1,29 @@
-let arr = [1, 2, 3, 5]
-let n = 2
-
-function z(arr, n) {
+document.getElementById('task20__button').onclick = () => {
     let c = []
+    let arr = document.getElementById('task20__arr').value
+    arr = arr.split(' ')
+    let n = Number(document.getElementById('task20__number').value)
+
     for (let i = 0; i <= arr.length - n; i++) {
         let chunk = []
-        for (let j = i; j < i+ n; j++) {
+        for (let j = i; j < i + n; j++) {
             chunk.push(arr[j])
         }
         c.push(chunk)
     }
-    console.log(c)
+    // console.log(c[0].length)
+    let text = ''
+    for (let i = 0; i < c.length; i++) {
+        text += ' {'
+        for (let j = 0; j < c[i].length; j++) {
+            if (j !== c[i].length - 1) {
+                text += `${c[i][j]}, `
+            } else {
+                text += `${c[i][j]}`
+            }
+        }
+        text += '}'
+    }
+    document.getElementById('task20__resalt').value = text
 }
 
-z(arr, n)
