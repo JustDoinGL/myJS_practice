9514e2ad7f6c29e9d3c4edf6f5fca7f52dfb40f4
Andrew <106160775+JustDoinGL@users.noreply.github.com>
2023-03-14 19:31:38 +0300

    Add files via upload

--- js/21.js
+++ js/21.js
@@ -1,14 +1,14 @@
-let n1 = 0
-let n2 = 10
-
-function n() {
-    let n1 = 0
-    let n2 = 10
-    let y = []
-    for (let i = n1; i < n2; i++) {
-        y.push(i)
+document.getElementById('task21__button').onclick = () => {
+    let n1 = Number(document.getElementById('task21__firstNumber').value)
+    let n2 = Number(document.getElementById('task21__secondNumber').value)
+    let allNumbers = []
+    if (n1 >= n2-1) {
+        document.getElementById('task21__output').innerHTML = 'Вы ввели склом маленькое чвторое число.'
+    } else {   
+    for (let i = n1+1; i < n2; i++) {
+        allNumbers.push(i)
     }
-    console.log(y)
+    document.getElementById('task21__output').innerText = allNumbers
+    }
 }
 
-n()
