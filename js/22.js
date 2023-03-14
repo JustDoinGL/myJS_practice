400ce5bf49d60ac2f02fc386b9a0fa5dad38e098
Andrew <106160775+JustDoinGL@users.noreply.github.com>
2023-03-14 19:54:54 +0300

    Add files via upload

--- js/22.js
+++ js/22.js
@@ -1,8 +1,28 @@
-function n() {
-    let a = "a s c v"
-    let b = 's'
-    z = a.split(' ').map(el => b).join(' ')
-    console.log(z)
+document.getElementById('task22__button').onclick = () => {
+    let a = document.getElementById('task22__article').value
+    let b = document.getElementById('task22__change').value
+    if (a === '') {
+        document.getElementById('task22__output').innerHTML = 'Вы не ввели предложение'
+    } else if (b === '') {
+        document.getElementById('task22__output').innerHTML = 'Вы не ввели слово на котрое заменить предложение'
+    } else if (b === '' && a === '') {
+        document.getElementById('task22__output').innerHTML = 'У вас пустое слово и предложение'
+    } else {
+        out = a.split(' ').map(el => b).join(' ')
+        document.getElementById('task22__output').innerHTML = out
+    }
 }
 
-n()
+
+
+
+
+
+// function n() {
+//     let a = "a s c v"
+//     let b = 's'
+//     z = a.split(' ').map(el => b).join(' ')
+//     console.log(z)
+// }
+
+// n()
