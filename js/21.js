7899dc3cc232cff54c5b5a90ac0c0dc3ea73475b
Andrew <106160775+JustDoinGL@users.noreply.github.com>
2023-03-14 19:23:09 +0300

    Add files via upload

---
+++ js/21.js
@@ -0,0 +1,14 @@
+let n1 = 0
+let n2 = 10
+
+function n() {
+    let n1 = 0
+    let n2 = 10
+    let y = []
+    for (let i = n1; i < n2; i++) {
+        y.push(i)
+    }
+    console.log(y)
+}
+
+n()
