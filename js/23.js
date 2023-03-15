2a1dfb3245d7cb0643c64faf83a33894997aac8d
Andrew <106160775+JustDoinGL@users.noreply.github.com>
2023-03-15 15:56:32 +0300

    Add files via upload

--- js/23.js
+++ js/23.js
@@ -2,6 +2,7 @@ let input = document.querySelector('#task23__input')
 let send = document.querySelector('#task23__text')
 let li = document.querySelector('#task23__li')
 let form = document.querySelector('#task23__form')
+let pattern = /^[a-zA-Z0-9]+$/;
 
 
 send.addEventListener('click', formHadler)
@@ -9,24 +10,29 @@ send.addEventListener('click', formHadler)
 function formHadler() {
     let text = input.value
 
-    // Создаем элемент ли
-    let task = document.createElement('li')
-    task.innerText = text
-    li.append(task)
+    if (pattern.test(text)) {
+        // Создаем элемент ли
+        let task = document.createElement('li')
+        task.innerText = text
+        li.append(task)
 
-    // Создаем кнопку дилит, стили дла нее и добавляем в li
-    let deleteButton = document.createElement('button')
-    deleteButton.setAttribute('role', 'button')
-    deleteButton.innerText = "Удалить"
-    deleteButton.style['margin-left'] = '15px'
-    task.append(deleteButton)
+        // Создаем кнопку дилит, стили дла нее и добавляем в li
+        let deleteButton = document.createElement('button')
+        deleteButton.setAttribute('role', 'button')
+        deleteButton.innerText = "Удалить"
+        deleteButton.style['margin-left'] = '15px'
+        task.append(deleteButton)
 
-    // Вешаем листенер по нажатию
-    deleteButton.addEventListener('click', function () {
-        this.closest('li').remove()
-    })
+        // Вешаем листенер по нажатию
+        deleteButton.addEventListener('click', function () {
+            this.closest('li').remove()
+        })
 
-    // Обнуляем поле ввода
-    input.value = ''
+        // Обнуляем поле ввода
+        input.value = ''
+    } else {
+        let info = document.createElement('p')
+        info.innerText = "Ваша задача пуста, введите хотя ы один символ"
+    }
 
 }
