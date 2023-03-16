554b759c23200d079c39421dee9559cc7329d424
Andrew <106160775+JustDoinGL@users.noreply.github.com>
2023-03-16 19:54:21 +0300

    Add files via upload

--- js/24.js
+++ js/24.js
@@ -10,17 +10,28 @@ let button = document.querySelector('#task24__button')
 button.addEventListener('click', outputText)
 
 function outputText() {
-    let startTime = Number(document.querySelector('#task24__start').value) * 1000
-    let finichTime = Number(document.querySelector('#task24__stop').value) * 1000
-    let text = document.querySelector('#task24__text').value
-    let outputText = document.querySelector('#task24__output')
+     // Объявляем переменные
+     let startTime = Number(document.querySelector('#task24__start').value) * 1000
+     let finichTime = Number(document.querySelector('#task24__stop').value) * 1000
+     let text = document.querySelector('#task24__text').value
+     let outputText = document.querySelector('#task24__output')
 
-    console.log(finichTime)
+    // Обнудяем вывод текста
     
-    // Cоздаем интревал и выводим данные через таймер 
-    const intervalID = setInterval(() => outputText.innerHTML += text, startTime)
+        outputText.innerHTML = ""
 
-     // Cоздаем тамаут и останвливаем вывод данных 
-    setTimeout(()=> {clearInterval(intervalID)}, finichTime)
+    // console.log(finichTime)
+
+    if (startTime >= finichTime) {
+        outputText.innerHTML = "У Вас ничего не выведится, так как первый интервал меньше или равен второму."
+    } else {
+        // Cоздаем интревал и выводим данные через таймер 
+        const intervalID = setInterval(() => outputText.innerHTML += `${text}, `, startTime)
+
+        // Cоздаем тамаут и останвливаем вывод данных 
+        setTimeout(() => { clearInterval(intervalID) 
+            outputText.innerHTML += "Вывод закончен."
+        }, finichTime)
+    }
 
 }
