1e0e5c0b05afb3583be25de61694e173630569f1
Andrew <106160775+JustDoinGL@users.noreply.github.com>
2023-03-16 19:41:03 +0300

    Add files via upload

--- js/24.js
+++ js/24.js
@@ -1,6 +1,26 @@
-let startTime = 400
-let finichTime = 20000
+// let startTime = 400
+// let finichTime = 20000
 
-const intervalID = setInterval(() => console.log(2), startTime)
+// const intervalID = setInterval(() => console.log(2), startTime)
 
-setTimeout(()=> {clearInterval(intervalID)}, finichTime)
+// setTimeout(()=> {clearInterval(intervalID)}, finichTime)
+
+let button = document.querySelector('#task24__button')
+
+button.addEventListener('click', outputText)
+
+function outputText() {
+    let startTime = Number(document.querySelector('#task24__start').value) * 1000
+    let finichTime = Number(document.querySelector('#task24__stop').value) * 1000
+    let text = document.querySelector('#task24__text').value
+    let outputText = document.querySelector('#task24__output')
+
+    console.log(finichTime)
+    
+    // Cоздаем интревал и выводим данные через таймер 
+    const intervalID = setInterval(() => outputText.innerHTML += text, startTime)
+
+     // Cоздаем тамаут и останвливаем вывод данных 
+    setTimeout(()=> {clearInterval(intervalID)}, finichTime)
+
+}
