cdb3e6398e975fb7de55664c03474b66f89e9d11
Andrew <106160775+JustDoinGL@users.noreply.github.com>
2023-03-17 20:12:06 +0300

    Add files via upload

--- js/25.js
+++ js/25.js
@@ -4,7 +4,7 @@ const text = document.querySelector('#task25__span')
 button2.addEventListener('click', pow)
 
 function pow() {
-    let c = 1
+    text.innerHTML = ''
     let arr1 = []
     const arr = document.querySelector('#task25__arr').value
     let z = ''
@@ -12,14 +12,31 @@ function pow() {
         if (arr[i] === ' ') {
             arr1.push((Number(z)))
             z = ''
-        } else if (i+1 === arr.length) {
+        } else if (i + 1 === arr.length) {
             z += arr[i]
             arr1.push((Number(z)))
         } else {
             z += arr[i]
         }
     }
-    console.log(arr1)
-    arr1.forEach((el) => c *= el )
-    text.innerHTML = c
+    let q = pow2(arr1)
+    if (isNaN(q)) {
+        text.innerHTML = 'Вы ввели не только одни цифры, умножение невозможно'
+    } else {
+        text.innerHTML = q
+    }
+}
+
+function pow2(arr1) {
+    let c = 1
+    arr1.forEach(element => {
+        if (typeof element === 'Null' ) {
+            c = 'Вы ввели не только одни цифры, умножение невозможно'
+            return c
+        } else {
+            c *= element
+        }
+        console.log(element)
+    });
+    return c
 }
