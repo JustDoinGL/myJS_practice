add9037862deb2bd2f3059d47d381e5faf3e7431
Andrew <106160775+JustDoinGL@users.noreply.github.com>
2023-03-17 19:40:27 +0300

    Add files via upload

--- js/25.js
+++ js/25.js
@@ -1,6 +1,25 @@
-const arr = [1, 2, 3]
-let c = 1
+let button2 = document.querySelector('#task25__button')
+const text = document.querySelector('#task25__span')
 
-arr.forEach((el) => c *= el )
+button2.addEventListener('click', pow)
 
-console.log(c)
+function pow() {
+    let c = 1
+    let arr1 = []
+    const arr = document.querySelector('#task25__arr').value
+    let z = ''
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] === ' ') {
+            arr1.push((Number(z)))
+            z = ''
+        } else if (i+1 === arr.length) {
+            z += arr[i]
+            arr1.push((Number(z)))
+        } else {
+            z += arr[i]
+        }
+    }
+    console.log(arr1)
+    arr1.forEach((el) => c *= el )
+    text.innerHTML = c
+}
