9d5cd81fd3e6309a99c9f7dbef93c1691f2873d4
Andrew <106160775+JustDoinGL@users.noreply.github.com>
2023-03-18 20:42:03 +0300

    Add files via upload

--- js/26.js
+++ js/26.js
@@ -39,8 +39,49 @@ function getValue() {
     const firstNumber = Number(document.querySelector('#task26__1number').value)
     const secondNumber = Number(document.querySelector('#task26__2number').value)
     const action = document.querySelector('#task26__action')
-    let selectedOption = action.selectedIndex
+    const selectedOption = action.selectedIndex
+    const text = document.querySelector('#task26__span')
 
-    if ()
+    // Обнуляем место для вывода ответа
+    text.innerHTML = ''
 
+    // Проверям, какое действие делать по index
+    if (selectedOption === 0) {
+        const total = multiply(firstNumber, secondNumber)
+        text.innerHTML = total
+    } else if (selectedOption === 1) {
+        const total = add(firstNumber, secondNumber)
+        text.innerHTML = total
+    }
+    else if (selectedOption === 2) {
+        const total = divide(firstNumber, secondNumber)
+        text.innerHTML = total
+    } else {
+        const total = subtract(firstNumber, secondNumber)
+        text.innerHTML = total
+    }
+}
+
+// функция умножения
+function multiply(firstNumber, secondNumber) {
+    return firstNumber * secondNumber
+}
+
+// функция сложения
+function add(firstNumber, secondNumber) {
+    return firstNumber + secondNumber
+}
+
+// функция деления
+function divide(firstNumber, secondNumber) {
+    if (secondNumber === 0) {
+        return "Делить на 0 нельзя"
+    } else {
+        return firstNumber / secondNumber
+    }
+}
+
+// функция вычетания
+function subtract(firstNumber, secondNumber) {
+    return firstNumber - secondNumber
 }
