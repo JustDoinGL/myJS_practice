2d0a2ab94266151416c0b54acb8091a8d19ee9fc
Andrew <106160775+JustDoinGL@users.noreply.github.com>
2023-03-18 21:02:20 +0300

    Add files via upload

--- js/26.js
+++ js/26.js
@@ -52,12 +52,20 @@ function getValue() {
     } else if (selectedOption === 1) {
         const total = add(firstNumber, secondNumber)
         text.innerHTML = total
-    }
-    else if (selectedOption === 2) {
+    } else if (selectedOption === 2) {
         const total = divide(firstNumber, secondNumber)
         text.innerHTML = total
-    } else {
+    } else if (selectedOption === 3) {
         const total = subtract(firstNumber, secondNumber)
+        text.innerHTML = total
+    } else if (selectedOption === 4) {
+        const total = divideNoMod(firstNumber, secondNumber)
+        text.innerHTML = total
+    } else if (selectedOption === 5) {
+        const total = divideNoMod(firstNumber, secondNumber)
+        text.innerHTML = total
+    } else {
+        const total = module(firstNumber, secondNumber)
         text.innerHTML = total
     }
 }
@@ -85,3 +93,28 @@ function divide(firstNumber, secondNumber) {
 function subtract(firstNumber, secondNumber) {
     return firstNumber - secondNumber
 }
+
+// функция деления без остатка
+function divideMod(firstNumber, secondNumber) {
+    if (secondNumber === 0) {
+        return "Делить на 0 нельзя"
+    } else {
+        return Math.floor(firstNumber / secondNumber)
+    }
+}
+
+// функция деления с остатком
+function divideNoMod(firstNumber, secondNumber) {
+    if (secondNumber === 0) {
+        return "Делить на 0 нельзя"
+    } else {
+        return firstNumber % secondNumber
+    }
+}
+
+// функция деления с модуль
+function module(firstNumber, secondNumber) {
+    return `${Math.abs(firstNumber)}, ${Math.abs(secondNumber)}`
+}
+
+
