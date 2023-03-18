49a293986a282fcb38a2634fdee4817853990ef6
Andrew <106160775+JustDoinGL@users.noreply.github.com>
2023-03-18 20:31:53 +0300

    Add files via upload

--- js/26.js
+++ js/26.js
@@ -1,33 +1,46 @@
-let firstNumber = 2
-let secondNumber = 5
+// let firstNumber = 2
+// let secondNumber = 5
 
-// функция умножения
-function multiply(firstNumber, secondNumber) {
-    return firstNumber * secondNumber
+// // функция умножения
+// function multiply(firstNumber, secondNumber) {
+//     return firstNumber * secondNumber
+// }
+
+// // функция сложения
+// function add(firstNumber, secondNumber) {
+//     return firstNumber + secondNumber
+// }
+
+// // функция деления
+// function divide(firstNumber, secondNumber) {
+//     if (secondNumber === 0) {
+//         return "Делить на 0 нельзя"
+//     } else {
+//         return firstNumber / secondNumber
+//     }
+// }
+
+// // функция вычетания
+// function subtract(firstNumber, secondNumber) {
+//     return firstNumber - secondNumber
+// }
+
+// subtract(firstNumber, secondNumber)
+// divide(firstNumber, secondNumber)
+// multiply(firstNumber, secondNumber)
+// add(firstNumber, secondNumber)
+
+const actionButton = document.querySelector('#task26__button')
+
+actionButton.addEventListener('click', getValue)
+
+function getValue() {
+    //Объявляем переменные
+    const firstNumber = Number(document.querySelector('#task26__1number').value)
+    const secondNumber = Number(document.querySelector('#task26__2number').value)
+    const action = document.querySelector('#task26__action')
+    let selectedOption = action.selectedIndex
+
+    if ()
+
 }
-
-// функция сложения
-function add(firstNumber, secondNumber) {
-    return firstNumber + secondNumber
-}
-
-// функция деления
-function divide(firstNumber, secondNumber) {
-    if (secondNumber === 0) {
-        return "Делить на 0 нельзя"
-    } else {
-        return firstNumber / secondNumber
-    }
-}
-
-// функция вычетания
-function subtract(firstNumber, secondNumber) {
-    return firstNumber - secondNumber
-}
-
-subtract(firstNumber, secondNumber)
-divide(firstNumber, secondNumber)
-multiply(firstNumber, secondNumber)
-add(firstNumber, secondNumber)
-
-
