1737dc1485aab30efb5d80ca95931f285cb24080
Andrew <106160775+JustDoinGL@users.noreply.github.com>
2023-03-19 20:38:56 +0300

    Add files via upload

---
+++ js/27.js
@@ -0,0 +1,6 @@
+const startButton = document.querySelector('#task27__start') 
+const pauseButton = document.querySelector('#task27__pause') 
+const resetButton = document.querySelector('#task27__reset') 
+const getTime = Number(document.querySelector('#task27__time'))
+
+// 
