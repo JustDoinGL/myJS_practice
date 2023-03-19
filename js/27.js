8e260f0062ff16d605976df3ce086302227c228c
Andrew <106160775+JustDoinGL@users.noreply.github.com>
2023-03-19 21:01:33 +0300

    Add files via upload

--- js/27.js
+++ js/27.js
@@ -1,6 +1,23 @@
-const startButton = document.querySelector('#task27__start') 
-const pauseButton = document.querySelector('#task27__pause') 
-const resetButton = document.querySelector('#task27__reset') 
-const getTime = Number(document.querySelector('#task27__time'))
+const startButton = document.querySelector('#task27__start')
+const pauseButton = document.querySelector('#task27__pause')
+const resetButton = document.querySelector('#task27__reset')
+let startTime;
 
-// 
+// Start
+startButton.onclick = () => {
+    let getTime = Number(document.querySelector('#task27__time').innerHTML)
+    startTime = setInterval(() => {
+        getTime++
+        document.querySelector('#task27__time').innerHTML = getTime
+    }, 1000)
+}
+
+// Pause
+pauseButton.onclick = () => clearTimeout(startTime)
+
+
+// Reset
+resetButton.onclick = () => {
+    clearTimeout(startTime)
+    document.querySelector('#task27__time').innerHTML = 0
+}
